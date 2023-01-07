cc7bd2e50b784e7d3adcf0d87101bf20988a7290
linszay <lindseyjlancaster@gmail.com>
2023-01-07 11:35:58 -0600

    using slice indexes to show parts of the word

---
+++ 0x00-python-hello_world/1-edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+word = "Holberton"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print("First 3 letters: {word_first_3}".format(word_first_3 = word[:3]))
+print("Last 2 letters: {word_last_2}".format(word_last_2 = word[7:]))
+print("Middle word: {middle_word}".format(middle_word = word[1:8]))
