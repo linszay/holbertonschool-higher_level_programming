d8c6e65d5d695de9a194bebe2d07895832b36d56
linszay <lindseyjlancaster@gmail.com>
2023-01-09 16:19:43 -0600

    modified condition for negative numbers

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = str(number)[-1]
+if (number < 0):
+    number = -number
 print('Last digit of {} is {} '.format(number, last_digit), end='')
 if (number > 5):
     print('and is greater than 5')
