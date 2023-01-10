1cbc331f89cbd432f0c3b8812681eda511249940
linszay <lindseyjlancaster@gmail.com>
2023-01-09 21:55:26 -0600

    prints numbers o to 99

---
+++ 0x00-python-hello_world/5-print_comb2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+numbers = range(0, 100)
+for i in numbers:
+    if (i < 10):
+        print('0{}, '.format(i))
+    elif (i >= 10 & i < 99):
+        print('{}, '.format(i))
+    else:
+        print('{}, '.format(i))
