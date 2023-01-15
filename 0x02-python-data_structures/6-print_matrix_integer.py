6ba0e3c7a487df517c13258554b328c00c0138e1
linszay <lindseyjlancaster@gmail.com>
2023-01-15 16:50:53 -0600

    removed first .join

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print('\n'.join([''.join(['{:d} '.format(item) for item in row])]))
-        if row == len(row)-1:
-            print(" ")
-        else:
-            print("")
+        print((' '.join(['{:d}'.format(item) for item in row])))
