b31b34a204f411dac054b4068ceed46b59e028f0
linszay <lindseyjlancaster@gmail.com>
2023-01-15 16:26:03 -0600

    added if else statement

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -2,3 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         print('\n'.join([''.join(['{:d} '.format(item) for item in row])]))
+        if row == len(row)-1:
+            print(" ")
+        else:
+            print("")
