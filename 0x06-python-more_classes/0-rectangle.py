f2a6cb5ad5eb93e536c62fa46ac95f3d0bd11c94
linszay <lindseyjlancaster@gmail.com>
2023-01-24 13:25:11 -0600

    changed width and height to value

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -14,9 +14,9 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if not isinstance(width, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -26,8 +26,8 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if not isinstance(height, int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise TypeError("height must be >= 0")
         self.__height = value
