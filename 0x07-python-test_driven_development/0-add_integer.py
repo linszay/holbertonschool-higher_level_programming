bfcd8524cafe91751b994834251e34d49c06fa1b
linszay <lindseyjlancaster@gmail.com>
2023-01-24 20:50:10 -0600

    added test file

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    if not isinstance(float, a) or not isinstance(int, a):
+        raise TypeError('a must be an integer')
+    if not isinstance(float, b) or not isinstance(int, b):
+        raise TypeError('b must be an integer')
+    else:
+        return (a + b)
