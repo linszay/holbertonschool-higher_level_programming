690b80f64340f03ddaf635e35ca46d6cc9b6331a
linszay <lindseyjlancaster@gmail.com>
2023-01-25 11:37:55 -0600

    modified test doc and .py file for checker

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,9 +4,8 @@
 
 def add_integer(a, b=98):
     """adds two integers."""
-    if not isinstance(float, a) or not isinstance(int, a):
+    if not isinstance(a, float) and not isinstance(a, int):
         raise TypeError('a must be an integer')
-    if not isinstance(float, b) or not isinstance(int, b):
+    if not isinstance(b, float) and not isinstance(b, int):
         raise TypeError('b must be an integer')
-    else:
-        return (int(a) + int(b))
+    return (int(a) + int(b))
