d7e3ec63724d6ab6107afabcf6275e765db0595b
linszay <lindseyjlancaster@gmail.com>
2023-01-25 19:10:45 -0600

    added unittests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,9 +10,24 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_list(self):
         ordered = [1, 2, 3, 4]
-        self.assertEqual(max_integer, 4)
+        self.assertEqual(max_integer(ordered), 4)
 
     def test_empty_list(self):
         """Test an empty list."""
         empty = []
         self.assertEqual(max_integer(empty), None)
+
+    def test_max_at_begginning(self):
+        """Test a list with a beginning max value."""
+        max_at_beginning = [4, 3, 2, 1]
+        self.assertEqual(max_integer(max_at_beginning), 4)
+
+    def test_one_element_list(self):
+        """Test a list with a single element."""
+        one_element = [3]
+        self.assertEqual(max_integer(one_element), 3)
+
+    def test_oneNeg_list(self):
+        oneNeg = [1, -2, 3, 4]
+        self.assertEqual(max_integer(oneNeg), 4)
+
