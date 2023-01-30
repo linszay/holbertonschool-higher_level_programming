71caab3216485386dc5afd567f5255746f11f808
linszay <lindseyjlancaster@gmail.com>
2023-01-30 17:13:13 -0600

    returns using isinstance to check is obj is instace of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""check if the obj is an instance of,
+    or if the object is an instance of a class
+    that inherited from, the specified class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """return using isinstance to check"""
+    return isinstance(obj, a_class)
