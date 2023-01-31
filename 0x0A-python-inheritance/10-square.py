d3f0fc69a196c69ca07dd6b0fd9ffe95dba97003
linszay <lindseyjlancaster@gmail.com>
2023-01-31 01:56:22 -0600

    added missing documentation

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,6 +4,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """creating a new class"""
     def __init__(self, size):
         self.integer_validator("size", size)
         super().__init__(size, size)
