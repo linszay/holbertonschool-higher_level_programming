79c11324f8abff183b23060bac105edea849d58d
linszay <lindseyjlancaster@gmail.com>
2023-01-30 21:29:36 -0600

    added missing documentation

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,6 +6,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """import base case to use integer_validator"""
     def __init__(self, width, height):
         self.__width = width
         self.__height = height
