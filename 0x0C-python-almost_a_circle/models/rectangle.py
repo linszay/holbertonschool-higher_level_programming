fd10e1d5f1088ed964d1fb2abf11078d0d442270
linszay <lindseyjlancaster@gmail.com>
2023-01-31 17:10:36 -0600

    added more methods

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -59,3 +59,9 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
