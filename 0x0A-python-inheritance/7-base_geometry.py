c02084d8590370dbdc67aae99e1b0b7b87ac0d23
linszay <lindseyjlancaster@gmail.com>
2023-01-30 20:44:46 -0600

    removed whitespace on 7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,7 +6,7 @@ class BaseGeometry():
     """adding more instances"""
     def area(self):
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
