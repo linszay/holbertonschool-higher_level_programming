8401ac1831760119f5b5ca524382548dd184d1d2
linszay <lindseyjlancaster@gmail.com>
2023-01-30 21:46:13 -0600

    corrected integer_validator args

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -10,5 +10,5 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         self.__width = width
         self.__height = height
-        self.integer_validator("width", int)
-        self.integer_validator("height", int)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
