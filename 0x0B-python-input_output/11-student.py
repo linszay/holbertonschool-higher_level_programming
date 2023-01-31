f06ab4d204eee1d7acf55afc90f3bcbdcd98242f
linszay <lindseyjlancaster@gmail.com>
2023-01-31 15:49:27 -0600

    addednew method

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""adding new method"""
+
+
+class Student:
+    """creating new method"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return {key: value for key, value in self.__dict__.items()}
+        else:
+            data = {}
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    data[key] = value
+            return data
+
+    def reload_from_json(self, json):
+        for k, v in json.items():
+            setattr(self, k, v)
