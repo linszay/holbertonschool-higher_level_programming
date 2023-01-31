c9d856fc74f73af5228b5cd30a2e654cbbac9f1a
linszay <lindseyjlancaster@gmail.com>
2023-01-30 20:22:39 -0600

    added instance to class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""adding to class BaseGeometry"""
+
+
+class BaseGeometry():
+    """adding to the class"""
+    def area(self):
+        raise Exception("area() is not implemented")
