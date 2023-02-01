1e7b1c433d5e1b28b2c87c125e66fe8b8b1709a9
linszay <lindseyjlancaster@gmail.com>
2023-01-31 19:53:24 -0600

    added import json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """creating Base class"""
+import json
 
 
 class Base():
@@ -13,7 +14,7 @@ class Base():
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-     @staticmethod
+    @staticmethod
     def to_json_string(list_dictionaries):
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
