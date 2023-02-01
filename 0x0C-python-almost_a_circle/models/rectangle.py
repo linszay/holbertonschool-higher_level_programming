8389dffd0bb3a8fbb02de2c3fd5f63e4d0626383
linszay <lindseyjlancaster@gmail.com>
2023-01-31 19:43:35 -0600

    retesting for checker

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -20,7 +20,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """width setter"""
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -34,7 +34,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """height setter"""
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -48,7 +48,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """x setter"""
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -62,7 +62,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """y setter"""
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
@@ -85,6 +85,7 @@ class Rectangle(Base):
         value += "{}/{} - ".format(self.x, self.y)
         value += "{}/{}".format(self.width, self.height)
         return value
+        
     def update(self, *args, **kwargs):
         """ Assigns key/value arguments to attributes """
         if args:
