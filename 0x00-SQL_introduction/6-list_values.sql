15e8a59adc14bdfd811f4ee62871dbbd229400e4
linszay <lindseyjlancaster@gmail.com>
2023-02-13 11:43:49 -0600

    added missing - to comment

--- 0x00-SQL_introduction/6-list_values.sql
+++ 0x00-SQL_introduction/6-list_values.sql
@@ -1,4 +1,5 @@
-- lists all rows of the
-table
-    first_table -- all fields should be printed
+-- lists all rows of the table first_table
+
+-- all fields should be printed
+
 SELECT * FROM first_table;
