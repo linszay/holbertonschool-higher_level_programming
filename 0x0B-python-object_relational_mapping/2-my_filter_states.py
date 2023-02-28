b549c8e9206cfeaa394ae5a61f40c6c9a8e792fc
linszay <lindseyjlancaster@gmail.com>
2023-02-27 23:33:08 -0600

    displays state value that matches the arg provided

---
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+lists state name matching argv[4] from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connecting to the database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    # Creating a cursor object
+    cursor = db.cursor()
+
+    # Executing the query
+    cursor.execute(
+        "SELECT * FROM states WHERE name='{}' \
+        ORDER BY id ASC".format(state_name))
+
+    # Retrieving the results
+    results = cursor.fetchall()
+
+    # Displaying the results
+    for state in results:
+        if state[0] <= 5:
+            print(state)
+
+    # Closing the connection to the database
+    db.close()
