68b35ad56ce08c909c3cb550106e37a51f81d6d3
linszay <lindseyjlancaster@gmail.com>
2023-02-28 05:01:57 -0600

    removed duplicate sys.argv

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -7,9 +7,6 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
     state_name = sys.argv[4]
 
     # Connecting to the database
