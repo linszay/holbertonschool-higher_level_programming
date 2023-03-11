c0656bde343b2c799746b5514efcc86837cee9a6
linszay <lindseyjlancaster@gmail.com>
2023-03-11 04:29:11 -0600

    added line 12

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -8,7 +8,9 @@ import urllib.response
 import urllib.request
 import sys
 
-url = sys.argv[1]
+
+if __name__ == "__main__":
+    url = sys.argv[1]
 
 with urllib.request.urlopen(url) as response:
     header = response.getheader('X-Request-Id')
