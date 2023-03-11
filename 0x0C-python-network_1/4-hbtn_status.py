7ccd2ea9689422cb26b1dfeec6b0540482d85094
linszay <lindseyjlancaster@gmail.com>
2023-03-11 09:10:16 -0600

    fixed last print statement

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     print("Body response:")
     print("\t- type:", type(response.content))
     print("\t- content:", response.content)
-    print("\t- utf8 content:", response.content.decode('utf-8'))
+    print("\t- content:", response.content.decode('utf-8'))
