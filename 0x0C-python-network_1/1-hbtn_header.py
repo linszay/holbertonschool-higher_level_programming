23ae5c39880a3ea2ecceba6712ca6470b804eafa
linszay <lindseyjlancaster@gmail.com>
2023-03-11 04:32:38 -0600

    removed addtional lines of comment to see if that fixes my issue

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""
-script takes in a URL, sends a request to the URL
-and displays the value of the X-Request-Id variable
-found in the header of the response
-"""
+"""displays the value of the X-Request-Id variable"""
 import urllib.response
 import urllib.request
 import sys
