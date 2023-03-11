127a42aaad79eba5880b965d1d5744ed78a4c8b6
linszay <lindseyjlancaster@gmail.com>
2023-03-11 04:36:13 -0600

    modified comment again trying to meet the req check

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-"""displays value found in header of response"""
+"""
+ script that takes in a URL,
+ sends a request to the URL and displays
+ the value of the X-Request-Id variable found
+ in the header of the response.
+"""
 import urllib.response
 import urllib
 import sys
