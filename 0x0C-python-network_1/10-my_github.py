e3bc0c78ab759e14e708271879fa9f9611a2f479
linszay <lindseyjlancaster@gmail.com>
2023-03-11 09:01:09 -0600

    script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id

---
+++ 0x0C-python-network_1/10-my_github.py
@@ -0,0 +1,25 @@
+#!usr/bin/python3
+"""
+script that takes your GitHub credentials (username
+and password) and uses the GitHub API to display your
+id
+"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    url = 'https://api.github.com/user'
+
+    auth = (username, password)
+
+    response = requests.get(url, auth=auth)
+
+    if response.status_code == 200:
+        data = response.json()
+        print("Your GitHub user ID is:", data['id'])
+    else:
+        print("Error retrieving GitHub user information. HTTP status code:",
+              response.status_code)
