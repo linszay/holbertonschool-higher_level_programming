04f62236e98a5540ca0bbab943e7a71454b144aa
linszay <lindseyjlancaster@gmail.com>
2023-03-11 04:42:18 -0600

    modified comment again and added beginning of 2-post_email

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
- script that takes in a URL,
- sends a request to the URL and displays
- the value of the X-Request-Id variable found
- in the header of the response.
+script that takes in a URL,
+sends a request to the URL and displays
+the value of the X-Request-Id variable found
+in the header of the response.
 """
 import urllib.response
 import urllib
