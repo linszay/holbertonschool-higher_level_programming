49d9aad6e9403296d8e6a7d0c799f32c6444ad3d
linszay <lindseyjlancaster@gmail.com>
2023-03-11 04:23:31 -0600

    moved comment down one line

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""script takes in a URL, sends a request to the URL
+"""
+script takes in a URL, sends a request to the URL
 and displays the value of the X-Request-Id variable
-found in the header of the response"""
+found in the header of the response
+"""
 from urllib import response
 import urllib.request
 import sys
