93f3294876bb80a4ce0023ab07f6f0269340cccf
linszay <lindseyjlancaster@gmail.com>
2023-03-15 11:25:58 -0500

    pushing file again to test

--- 0x0C-python-network_1/7-error_code.py
+++ 0x0C-python-network_1/7-error_code.py
@@ -6,10 +6,10 @@ and displays the body of the response
 import requests
 import sys
 
-if __name__ == "__name__":
-    response = requests.get(sys.argv[1])
 
-    if response.status_code >= 400:
-        print("Error code: {}".format(response.status_code))
+if __name__ == "__main__":
+    res = requests.get(sys.argv[1])
+    if res.status_code >= 400:
+        print("Error code: {}".format(res.status_code))
     else:
-        print(response.text)
+        print(res.text)
