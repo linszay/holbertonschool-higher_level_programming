0b195dfed8c06387ba1d040a6514d5f16a569886
linszay <lindseyjlancaster@gmail.com>
2023-03-14 23:36:55 -0500

    updated print statement

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     response = requests.get(url)
 
     print("Body response:")
-    print("\t- type:".format(type(response.content)))
+    print("\t- type:", (type(response.text)))
     print("\t- content:", response.content.decode('utf-8'))
