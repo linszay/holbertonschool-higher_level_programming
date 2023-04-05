8a43127eff8d4ea5ec7634f00e04a41197847bba
linszay <lindseyjlancaster@gmail.com>
2023-04-05 06:29:52 -0500

    searches second biggest int in list of args

--- 0x00-javascript-warm_up/11-second_biggest.js
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+const args = process.argv.slice(2);
+
+if (args.length < 2) {
+  console.log(0);
+} else {
+  let first = parseInt(args[0]);
+  let second = parseInt(args[1]);
+
+  if (second > first) {
+    [first, second] = [second, first]; // swap first and second
+  }
+
+  for (let i = 2; i < args.length; i++) {
+    const num = parseInt(args[i]);
+    if (num > first) {
+      second = first;
+      first = num;
+    } else if (num > second) {
+      second = num;
+    }
+  }
+
+  console.log(second);
+}
