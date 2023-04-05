c6e96289db981dd0bd91b7f0897066a337763595
linszay <lindseyjlancaster@gmail.com>
2023-04-05 04:19:19 -0500

    updated to fix semistandard

--- 0x00-javascript-warm_up/5-to_integer.js
+++ 0x00-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const arg = process.argv[2];
 if (!isNaN(arg)) {
-    console.log('My number: ' + parseInt(arg));
+  console.log('My number: ' + parseInt(arg));
 } else {
-    console.log('Not a number');
+  console.log('Not a number');
 }
